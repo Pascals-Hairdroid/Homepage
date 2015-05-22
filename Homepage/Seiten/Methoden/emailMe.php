5fc5fe7cf451d0b20eca22cd41200b455705f39d
MaxTop <MaxTop@Schullaptop>
2015-05-22 12:36:33 +0200

    Angebote.php,Produkte.php,Profil.php,dienstleistung.php,kontakt.php,offnungszeiten.php,studio.php,terminvergabe.php edited: Überprüft ob es per Webview oder nicht Webview aufgerufen wird;; ;; ;;; headerNoLink.html added: ein header ohne Verlinkung auf die Index;; ;; ;;; emailMe.php edited: es wird nun auch die emailadresse mitgesendet als get methode;; ;; ;;; forgotPassword.php edited: Token wird nun ins Profil gesetzt;; ;; ;;; passwortAendern.php edited: angepasst für get methode;; testen auf pascals.at;; ;;;

--- Homepage/Seiten/Methoden/emailMe.php
+++ Homepage/Seiten/Methoden/emailMe.php
@@ -2,7 +2,8 @@
 function sendEmailToken($empfaenger,$betreff,$Token){
 $from = "From: Pascals Hairstyle <Pascals.Hairstyle@gmail.com>\n";
 $from .= "Content-Type: text/html\n";
-$text = "Ihr Token:".$Token;
+$text = "Klicken Sie bitte auf diesen Link:\n";
+$text .= "www.pascals.at/v2/seiten/forgotPassword.php?tok=".$Token."&e=".$empfaenger;
 
 mail($empfaenger, $betreff, $text, $from);
 }
