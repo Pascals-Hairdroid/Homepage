919723a484df7aa9bd4165b2ce2b7acb62d1b254
MaxTop <MaxTop@10.91.54.158>
2015-06-11 09:43:04 +0200

    impressum.php edited: angepasst nach rechtlichen grundlagen;; ;; ;;;

--- Homepage/Seiten/Impressum.php
+++ Homepage/Seiten/Impressum.php
@@ -1,15 +1,16 @@
 <?php session_start();
-include("include_DBA.php");
+include("../include_DBA.php");
 $db=new db_con("conf/db.php",true);?>
 <!DOCTYPE html>
 <html>
 <head>
+
 <title>PASCALS HAIRSTYLE</title>
-<link rel='stylesheet' type='text/css' href='css/css.css'>
+<link rel='stylesheet' type='text/css' href='../css/css.css'>
 
 <?php 
 if(isset($_GET['web']))
-	echo "<link rel='stylesheet' type='text/css' href='css/hide.css'>";
+	echo "<link rel='stylesheet' type='text/css' href='../css/hide.css'>";
 ?>
 
 
@@ -26,80 +27,10 @@ if(isset($_GET['web']))
 				})
 		});
 		</script>
-<script language="javascript" type="text/javascript">
-		var wechselZeit = 2000; 
-   imageArr = new Array() 
-   imageArr[imageArr.length] = "Bilder/Bildlauf/6.jpg"; 
-   imageArr[imageArr.length] = "Bilder/Bildlauf/5.jpg"; 
-   imageArr[imageArr.length] = "Bilder/Bildlauf/4.jpg"; 
-   imageArr[imageArr.length] = "Bilder/Bildlauf/3.jpg"; 
-   imageArr[imageArr.length] = "Bilder/Bildlauf/2.jpg"; 
-   imageArr[imageArr.length] = "Bilder/Bildlauf/1.jpg"; 
-
-	
- 
-   var xAnzahl = imageArr.length; 
-   var xCounter=-1; 
-   var maxOpacity = 100; 
-   var minOpacity = 0; 
-   var disableOpacity = maxOpacity; 
-   var fadeInterval = 'fadeout'; 
-   function Vorladen() 
-   { 
-	   for (i = 0; i < imageArr.length; i++) 
-		   { var Bild = new Image(); Bild.src = imageArr[i]; } 
-	   }
- Vorladen();
-   function Bildwechsel01() 
-   { 
-    var objekt = document.getElementById('Foto01'); 
-    xCounter++; 
-    if (xCounter < xAnzahl) 
-    { 
-     if (fadeInterval == 'fadeout') {setOpacity(objekt, 'fadeout'); } 
-     if (disableOpacity < minOpacity) 
-     { 
-      objekt.src = imageArr[xCounter]; 
-      fadeInterval = 'fadein'; 
-     } 
-     if (fadeInterval == 'fadein') {setOpacity(objekt, 'fadein');} 
-     if (disableOpacity > maxOpacity) 
-     { 
-      fadeInterval = 'fadeout'; 
-      wechselZeit = 3000; 
-     } else { 
-      wechselZeit = 20; 
-     } 
-     setTimeout('Bildwechsel01()', wechselZeit); 
-    } else { 
-     xCounter = -1; 
-     Bildwechsel01(); 
-    } 
-   } 
-   setTimeout('Bildwechsel01()', wechselZeit); 
- 
-   function setOpacity(obj, direction) 
-   { 
-    obj.style.opacity = (disableOpacity / 100); 
-    obj.MozOpacity = (disableOpacity / 100); 
-    obj.style.filter = "alpha(opacity=" + disableOpacity + ")"; 
-    switch (direction) 
-    { 
-     case 'fadein': 
-      disableOpacity++; 
-      break; 
-     case 'fadeout': 
-      disableOpacity--; 
-      break; 
-    } 
-   } 
-</script>
-
-
 </head>
 <body>
 	<?php
-	include("Seiten/Anmeldung/login.php");
+	include("Anmeldung/login.php");
 	if(isset($_POST['submit'])){
 			$passwort = md5($_POST['passwort']);
 			$username=$_POST['username'];
@@ -165,7 +96,10 @@ if(isset($_GET['web']))
 		</div>
 		<div id="head">
 			<a href="#" style="color:black;"><h1>
-				PASCALS<img src="Bilder/Homepage/Logo.png"> HAIRSTYLE
+				<?php
+			if(isset($_GET['web']))include ("HTML/headerNoLink.html");
+			else include ("HTML/header.html");
+			?>
 			</h1>
 			<h2>Frisuren zum Wohlf&uuml;hlen</h2>
 		</a></div>
@@ -175,15 +109,15 @@ if(isset($_GET['web']))
 				<ul class="hide">
 					<li><a href="index.php" class="selected">Friseurstudio</a>
 						<ul>
-							<li><a href="Seiten/studio.php">Das Studio</a></li>
-							<li><a href="Seiten/team.php">Unser Team</a></li>
-							<li><a href="Seiten/dienstleistung.php">Dienstleistungen</a></li>
-							<li><a href="Seiten/offnungszeiten.php">&Ouml;ffnungszeiten</a></li>
-							<li><a href="Seiten/kontakt.php">Kontakt</a></li>
+							<li><a href="studio.php">Das Studio</a></li>
+							<li><a href="team.php">Unser Team</a></li>
+							<li><a href="dienstleistung.php">Dienstleistungen</a></li>
+							<li><a href="offnungszeiten.php">&Ouml;ffnungszeiten</a></li>
+							<li><a href="kontakt.php">Kontakt</a></li>
 						</ul>
 					</li>
-					<li><a href="Seiten/terminvergabe.php">Termine</a></li>
-					<li><a href="Seiten/Angebote.php">Angebote</a></li>
+					<li><a href="terminvergabe.php">Termine</a></li>
+					<li><a href="Angebote.php">Angebote</a></li>
 					<li class="topmenu"><a href="#"> Produkte</a>
 						<ul>
 							<?php 
@@ -206,43 +140,130 @@ if(isset($_GET['web']))
 
 
 		<div id="textArea">
-			<img class="titelbild" id="Foto01" src="Bilder/Bildlauf/1.jpg">
-			<h1>Willkommen bei Pascals Hairstyle!</h1>
-
-			<p>wenn du einen Stylisten suchst der sich Zeit nimmt, um wirklich
-				auf dich einzugehen und dir auch Tipps mitgibt, damit deine Frisur
-				auch in den Wochen nach dem Besuch optimal zur Geltung kommt, bist
-				du hier genau richtig.</p>
-
-			<p>jeder Mensch ist anders. Auch jeder Kopf ist anders. Wenn es darum
-				geht eine neue Frisur zu finden die individuell auf dich abgestimmt
-				ist, dann kann ich dir durch meine intensive Beratung und
-				langjÃ¤hrige Erfahrung beratend zur Seite stehen, sodass wir
-				gemeinsam zu einem Ergebnis gelangen, dass dich und damit auch mich
-				glÃ¼cklich macht.</p>
+		<br>
+			<h2>ADRESSDATEN</h2>
+			<p>
+				PASCALS HAIRSTYLE<br> 
+				Hairstyle Kagranerplatz GMBH<br>
+				Wagramerstrasse 154 a<br> 
+				1220 Wien
+			</p>
 			<br>
-
-
-			<p>Dabei sehe ich mich als deinen Partner der mit dir ein gemeinsames
-				Ziel verfolt: einen Style zu finden der dich begeistert und zu
-				deinem Wohlbefinden beitrÃ¤gt.</p>
+			<h2>KONTAKTDATEN</h2>
+			<p>
+				Telefon: +43676 92 38 217<br> 
+				E-Mail: office@pascals.at<br>
+				Internet: http://www.pascals.at<br>
+			</p>
 			<br>
-
-			<p>Wenn du einen Friseur suchst bei dem dein Wohlbefinden im
-				Vordergrund steht, bist du bei mir genau richtig. Ãœberzeuge dich
-				selbst davon und schau zu einem Beratungstermin vorbei.</p>
+			<h2>RECHTLICHE INFORMATIONEN</h2>
+			<p>
+		
+				<h3>Inhaber</h3>
+				<p>Herr Pascal Petritsch </p>
+				<h3>Rechtsform</h3>
+				<p>Gesellschaft mit beschränkter Haftung</p>
+				<h3>Berufsbezeichung</h3>
+				<p>Friseur, Österreich</p>
+				<h3>Umsatzsteuer ID</h3>
+				<p>12345678</p><br>
+			</p>
 			<br>
-
-			<p>Ich freue mich darauf schon bald mit dir zusammenzuarbeiten.</p>
-			<br>
-			<p>Pascal</p>
-		</div>
-
-		<div id="footer">
-			<a href="Seiten/Impressum.php">Impressum</a>
+			<p>
+			<h1 style="color:black;">HAFTUNGSAUSSCHLUSS</h1>
+			<br> 
+			<h2>1. Inhalt des Onlineangebotes</h2>
+			<p>
+			Der Autor übernimmt keinerlei Gewähr für die Aktualität, Korrektheit,
+			Vollständigkeit oder Qualität der bereitgestellten Informationen.
+			Haftungsansprüche gegen den Autor, welche sich auf Schäden
+			materieller oder ideeller Art beziehen, die durch die Nutzung oder
+			Nichtnutzung der dargebotenen Informationen bzw. durch die Nutzung
+			fehlerhafter und unvollständiger Informationen verursacht wurden,
+			sind grundsätzlich ausgeschlossen, sofern seitens des Autors kein
+			nachweislich vorsätzliches oder grob fahrlässiges Verschulden
+			vorliegt. Alle Angebote sind freibleibend und unverbindlich. Der
+			Autor behält es sich ausdrücklich vor, Teile der Seiten oder das
+			gesamte Angebot ohne gesonderte Ankündigung zu verändern, zu
+			ergänzen, zu löschen oder die Veröffentlichung zeitweise oder
+			endgültig einzustellen.</p>
+			<br><br>
+			<h2>2. Verweise und Links</h2>
+			<p>
+			Bei direkten oder indirekten Verweisen auf fremde Webseiten
+			("Hyperlinks"), die außerhalb des Verantwortungsbereiches des Autors
+			liegen, würde eine Haftungsverpflichtung ausschließlich in dem Fall
+			in Kraft treten, in dem der Autor von den Inhalten Kenntnis hat und
+			es ihm technisch möglich und zumutbar wäre, die Nutzung im Falle
+			rechtswidriger Inhalte zu verhindern. Der Autor erklärt hiermit
+			ausdrücklich, dass zum Zeitpunkt der Linksetzung keine illegalen
+			Inhalte auf den zu verlinkenden Seiten erkennbar waren. Auf die
+			aktuelle und zukünftige Gestaltung, die Inhalte oder die
+			Urheberschaft der verlinkten/verknüpften Seiten hat der Autor
+			keinerlei Einfluss. Deshalb distanziert er sich hiermit ausdrücklich
+			von allen Inhalten aller verlinkten /verknüpften Seiten, die nach der
+			Linksetzung verändert wurden. Diese Feststellung gilt für alle
+			innerhalb des eigenen Internetangebotes gesetzten Links und Verweise
+			sowie für Fremdeinträge in vom Autor eingerichteten Gästebüchern,
+			Diskussionsforen, Linkverzeichnissen, Mailinglisten und in allen
+			anderen Formen von Datenbanken, auf deren Inhalt externe
+			Schreibzugriffe möglich sind. Für illegale, fehlerhafte oder
+			unvollständige Inhalte und insbesondere für Schäden, die aus der
+			Nutzung oder Nichtnutzung solcherart dargebotener Informationen
+			entstehen, haftet allein der Anbieter der Seite, auf welche verwiesen
+			wurde, nicht derjenige, der über Links auf die jeweilige
+			Veröffentlichung lediglich verweist.</p>
+			<br><br>
+			<h2>3. Urheber- und Kennzeichenrecht</h2>
+			<p>Der Autor ist bestrebt, in allen Publikationen die Urheberrechte der
+			verwendeten Bilder, Grafiken, Tondokumente, Videosequenzen und Texte
+			zu beachten, von ihm selbst erstellte Bilder, Grafiken, Tondokumente,
+			Videosequenzen und Texte zu nutzen oder auf lizenzfreie Grafiken,
+			Tondokumente, Videosequenzen und Texte zurückzugreifen. Alle
+			innerhalb des Internetangebotes genannten und ggf. durch Dritte
+			geschützten Marken- und Warenzeichen unterliegen uneingeschränkt den
+			Bestimmungen des jeweils gültigen Kennzeichenrechts und den
+			Besitzrechten der jeweiligen eingetragenen Eigentümer. Allein
+			aufgrund der bloßen Nennung ist nicht der Schluss zu ziehen, dass
+			Markenzeichen nicht durch Rechte Dritter geschützt sind! Das
+			Copyright für veröffentlichte, vom Autor selbst erstellte Objekte
+			bleibt allein beim Autor der Seiten. Eine Vervielfältigung oder
+			Verwendung solcher Grafiken, Tondokumente, Videosequenzen und Texte
+			in anderen elektronischen oder gedruckten Publikationen ist ohne
+			ausdrückliche Zustimmung des Autors nicht gestattet.</p>
+			<br><br>
+			<h2>4. Datenschutz</h2>
+			<p>
+			Sofern innerhalb des Internetangebotes die Möglichkeit zur Eingabe
+			persönlicher oder geschäftlicher Daten (Emailadressen, Namen,
+			Anschriften) besteht, so erfolgt die Preisgabe dieser Daten seitens
+			des Nutzers auf ausdrücklich freiwilliger Basis. Die Inanspruchnahme
+			und Bezahlung aller angebotenen Dienste ist - soweit technisch
+			möglich und zumutbar - auch ohne Angabe solcher Daten bzw. unter
+			Angabe anonymisierter Daten oder eines Pseudonyms gestattet. Die
+			Nutzung der im Rahmen des Impressums oder vergleichbarer Angaben
+			veröffentlichten Kontaktdaten wie Postanschriften, Telefon- und
+			Faxnummern sowie Emailadressen durch Dritte zur Übersendung von nicht
+			ausdrücklich angeforderten Informationen ist nicht gestattet.
+			Rechtliche Schritte gegen die Versender von sogenannten Spam-Mails
+			bei Verstössen gegen dieses Verbot sind ausdrücklich vorbehalten.</p>
+			<br><br>
+			<h2>5. Rechtswirksamkeit dieses Haftungsausschlusses</h2>
+			<p>Dieser Haftungsausschluss ist als Teil des Internetangebotes zu
+			betrachten, von dem aus auf diese Seite verwiesen wurde. Sofern Teile
+			oder einzelne Formulierungen dieses Textes der geltenden Rechtslage
+			nicht, nicht mehr oder nicht vollständig entsprechen sollten, bleiben
+			die übrigen Teile des Dokumentes in ihrem Inhalt und ihrer Gültigkeit
+			davon unberührt.</p>
+			
+			
+	</div>
+	<div id="footer" align="center" class="hide">
+			<?php
+			include("HTML/footer.html");
+			?>
 		</div>
 	</div>
-
-	</div>
+	
 </body>
 </html>
