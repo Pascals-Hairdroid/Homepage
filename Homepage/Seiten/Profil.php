cb442ba77b07851c0a17187d76b288ca7476e30d
MaxTop <MaxTop@10.0.0.83>
2015-06-19 17:06:41 +0200

    notificationUpdate.php added: lässt die Notification bearbeiten;; ;; ;;; notification.php edited: small changes;; ;; ;;; notificationErstellen.php edited: kleinigkeiten beim array ausgebssert;; ;; ;;; profil.php edited: Link zum Passwort ändern implementiert;; ;; ;;;

--- Homepage/Seiten/Profil.php
+++ Homepage/Seiten/Profil.php
@@ -153,7 +153,8 @@ if(isset($_GET['web']))
 								echo umlaute_encode("<p><span class='font'>Nachname:</span>".$ma->getNachname()."</p>");
 								echo umlaute_encode("<p><span class='font'>Motto:</span>".$ma->getMotto()."</p>");
 								echo "<a class='font' href='ProfilBearbeiten.php?SVNr=".$ma->getSvnr()."'>Profil bearbeiten</a>";
-					}
+								echo "<a class='font' href='passwortAendern.php'>Passwort &aul;ndern</a>";
+}
 					else{
 						if($_SESSION['admin']==true){
 							$ma=$db->getMitarbeiter($_SESSION['svnr']);
@@ -173,7 +174,8 @@ if(isset($_GET['web']))
 			
 							echo umlaute_encode("<p><span class='font'>Motto:</span>".$ma->getMotto()."</p>");
 							echo "<a class='font' href='ProfilBearbeiten.php?SVNr=".$ma->getSvnr()."'>Profil bearbeiten</a>";
-						}
+							echo "<a class='font' href='passwortAendern.php'>Passwort &aul;ndern</a>";
+}
 						if($_SESSION['admin']==false){
 							$ku=$db->getKunde($_SESSION['email']);
 							$bildinfo = pathinfo($ku->getFoto());
@@ -190,6 +192,7 @@ if(isset($_GET['web']))
 							echo umlaute_encode("<p><span class='font'>Vorname:</span>".$ku->getVorname()."</p>");
 							echo umlaute_encode("<p><span class='font'>Nachname:</span>".$ku->getNachname()."</p>");
 							echo "<a class='font' href='ProfilBearbeiten.php?Email=".$ku->getEmail()."'>Profil bearbeiten</a>";
+							echo "<a class='font' href='passwortAendern.php'>Passwort &aul;ndern</a>";
 							
 						
 						}
